121803797e5e21b5666d8faa93a714a0572de27b
Maksym <zub.maksym@lll.kpi.ua>
2025-11-23 18:10:46 +0200

    remove border and add shadow

--- src/components/ui/CategoryCard.tsx
+++ src/components/ui/CategoryCard.tsx
@@ -6,7 +6,7 @@ const CategoryCard = ({ children, subtitle }: CategoryCardProps) => {
     return (
         <button className=''>
             <div
-                className={`sm:w-[200px] sm:h-[170px] w-[130px] h-[110px] rounded-xl cursor-pointer border overflow-hidden outline-offset-2 hover:outline-[#cddde4] hover:outline-3 hover:ring-2 hover:ring-[#4d6d7e]`}
+                className={`shadow-xl sm:w-[200px] sm:h-[170px] w-[130px] h-[110px] rounded-xl cursor-pointer overflow-hidden outline-offset-2 hover:outline-[#cddde4] hover:outline-3 hover:ring-2 hover:ring-[#4d6d7e]`}
             >
                 <div>
                     {children}
